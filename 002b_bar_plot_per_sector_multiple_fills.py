41d405052b1f92fc325f975b0f1e1b5fd78fa7ef
giadarol <giovanni.iadarola@cern.ch>
2019-04-05 14:41:10 +0200

    Status

--- 002b_bar_plot_per_sector_multiple_fills.py
+++ 002b_bar_plot_per_sector_multiple_fills.py
@@ -88,7 +88,7 @@ try:
 except Exception as err:
     print '\nDid not manage to set locale. Got:'
     print err
-    
+
 
 
 # build snapshots dicts
@@ -103,14 +103,14 @@ for strin in args.at:
             dd[kk] = float(value)
         else:
             raise ValueError("Input not recognized\n"+strin)
-            
+
     if 't_offs_h' not in dd.keys():
         dd['t_offs_h'] = None
-        
+
     snapshots.append(dd)
-    
-    
-    
+
+
+
 # merge pickles and add info on location
 dict_fill_bmodes={}
 for df in data_folder_list:
@@ -129,7 +129,7 @@ for i_snapshot in xrange(N_snapshots):
     filln = snapshots[i_snapshot]['filln']
     t_sample_h = snapshots[i_snapshot]['t_h']
     t_offset_h = snapshots[i_snapshot]['t_offs_h']
-    
+
     if from_csv:
         fill_file = 'fill_heatload_data_csvs/hl_all_cells_fill_%d.csv'%filln
         hid = tm.parse_timber_file(fill_file, verbose=args.v)
@@ -153,13 +153,13 @@ for i_snapshot in xrange(N_snapshots):
     else:
         # 2015 structure
         fill_dict.update(tm.parse_timber_file(data_folder_fill+'/fill_csvs/fill_%d.csv'%filln, verbose=args.v))
-        
+
     #sample standard fill data at right moment
     intensity_b1, intensity_b2, bl_ave_b1, bl_ave_b2, n_bunches_b1, n_bunches_b2, energy_GeV, hl_imped_sample, hl_sr_sample = cch.extract_and_compute_extra_fill_data(fill_dict, t_ref, t_sample_h, thresh_bint=3e10)
 
     # extract heat load data
     dict_hl_cell_by_cell = cch.sample_and_sort_cell_by_cell(hid, t_ref=t_ref, t_sample_h=t_sample_h, t_offset_h=t_offset_h, first_cell=args.first_cell)
-        
+
     snapshots[i_snapshot]['intensity_b1'] = intensity_b1
     snapshots[i_snapshot]['intensity_b2'] = intensity_b2
     snapshots[i_snapshot]['bl_ave_b1'] = bl_ave_b1
@@ -171,12 +171,12 @@ for i_snapshot in xrange(N_snapshots):
     snapshots[i_snapshot]['hl_imped_sample'] = hl_imped_sample
     snapshots[i_snapshot]['hl_sr_sample'] = hl_sr_sample
     snapshots[i_snapshot]['tref_string_short'] = tref_string_short
-    
+
     if t_offset_h is None:
         snapshots[i_snapshot]['t_offs_h_str'] = '-'
     else:
         snapshots[i_snapshot]['t_offs_h_str'] = '%.2f'%snapshots[i_snapshot]['t_offs_h']
-    
+
 # check consistency in cell naming
 for i_snapshot in xrange(N_snapshots):
     for s in hl.sector_list():
@@ -184,7 +184,7 @@ for i_snapshot in xrange(N_snapshots):
             raise ValueError('Found inconsistency type 1 in dict!')
         for c1, c2 in zip(snapshots[i_snapshot]['dict_hl_cell_by_cell'][s]['cell_names'], snapshots[0]['dict_hl_cell_by_cell'][s]['cell_names']):
             if c1!=c2:raise ValueError('Found inconsistency type 2 in dict!')
-       
+
 
 # Plots!
 plt.close('all')
@@ -203,23 +203,23 @@ y_list = []
 figlist = []
 
 for i, s in enumerate(hl.sector_list()):
-    
-    
+
+
     #single sector plot
     fig_sect = plt.figure(1000+i, figsize=(12,8.5), tight_layout=False)
     fig_sect.patch.set_facecolor('w')
     ax1_sect = plt.subplot2grid((2,2), (0,0), colspan=2, sharey=spshare)
     axhist = plt.subplot2grid((2,2), (1,0), colspan=1, sharey=spsharehist, sharex=spsharehist)
     spsharehist = axhist
-    
+
     sptable =  plt.subplot2grid((2,2), (1,1), colspan=1, sharey=spsharehist, sharex=spsharehist)
-    
+
     spshare = ax1_sect
-            
-    
-    y_list.append([])        
+
+
+    y_list.append([])
     for i_snapshot in xrange(N_snapshots):
-        
+
         this_hld = snapshots[i_snapshot]['dict_hl_cell_by_cell'][s]
         cells = this_hld['cell_names']
         hl_cells = this_hld['heat_loads'].copy()
@@ -236,10 +236,10 @@ for i, s in enumerate(hl.sector_list()):
                     label1, label2 = 'Imp', 'SR'
                 else:
                     label1, label2 = None, None
-                    
+
                 hl_imped_t1 = snapshots[i_snapshot]['hl_imped_sample']
                 hl_sr_t1 = snapshots[i_snapshot]['hl_sr_sample']
-    
+
                 ax1_sect.axhspan(ymin=0, ymax=hl_imped_t1/totintnorm, color='grey', alpha=0.5, label=label1)
                 ax1_sect.axhspan(ymin=hl_imped_t1/totintnorm, ymax=(hl_imped_t1+hl_sr_t1)/totintnorm, color='green', alpha=0.5, label=label2)
                 if args.legend:
@@ -251,41 +251,41 @@ for i, s in enumerate(hl.sector_list()):
         if normtointen:
             hl_cells/= totintnorm
 
-        ind = np.arange(len(cells)) 
+        ind = np.arange(len(cells))
         #alternating grey background
         if i_snapshot==0:
             for igrey in ind[1::2]:
                 ax1_sect.axvspan(igrey-0.5, igrey+0.5, color='k', alpha=.1)
-            
+
         #barplot
-        #~ ax1_sect.bar(ind-width/2+i_snapshot*width/N_snapshots, hl_cells, width/N_snapshots, 
+        #~ ax1_sect.bar(ind-width/2+i_snapshot*width/N_snapshots, hl_cells, width/N_snapshots,
             #~ color=colorleglist[i_snapshot], edgecolor=colorleglist[i_snapshot], linewidth = 0)
-        ax1_sect.bar(ind-width/2+i_snapshot*width/N_snapshots, hl_cells, width/N_snapshots, 
+        ax1_sect.bar(ind-width/2+i_snapshot*width/N_snapshots, hl_cells, width/N_snapshots,
             color=colorlist[i_snapshot], alpha=.5)
 
         if normtointen:
             ax1_sect.set_ylabel('Norm. heat load [W/hc/p+]')
         else:
             ax1_sect.set_ylabel('Heat load [W/hc]')
-            
-            
+
+
         ax1_sect.set_ylim(min_hl_scale, max_hl_scale)
-        
+
         ax1_sect.set_xticks(ind)
         ax1_sect.set_xticklabels(cells, rotation=90)
-        
+
         ax1_sect.set_xlim(ind[0]-2*width, ind[-1]+2*width)
 
         fig_sect.subplots_adjust(left=.06, right=.96, top=0.9, hspace=0.35, bottom=0.07)
-            
+
         ax1_sect.yaxis.grid(True)
-        
+
         # histogram
         histval, binedges = np.histogram(hl_cells, bins=nbinhist, range=(minhist, maxhist))
-        
+
         normhist = histval/(len(hl_cells)*np.mean(np.diff(binedges)))
         axhist.bar(left=binedges[:-1], width=np.diff(binedges), height=normhist, alpha=0.5, color=colorlist[i_snapshot], edgecolor='none')
-    
+
         if normtointen:
             axhist.set_xlabel('Norm. heat load [W/hc/p+]')
         else:
@@ -303,9 +303,9 @@ for i, s in enumerate(hl.sector_list()):
         axhist.grid('on')
         axhist.set_xlim(minhist, maxhist)
         axhist.ticklabel_format(style='sci', scilimits=(0,0),axis='y')
-        
+
         y_list[-1].append(tempy)
-        
+
     to_table = []
     to_table.append(['Fill'] + ['%d'%snapshots[i_snapshot]['filln'] for i_snapshot in xrange(N_snapshots)])
     to_table.append(['Started on'] + [snapshots[i_snapshot]['tref_string_short'] for i_snapshot in xrange(N_snapshots)])
@@ -329,8 +329,8 @@ for i, s in enumerate(hl.sector_list()):
     table.set_fontsize(myfontsz-1)
     fig_sect.suptitle('Sector %d, %d cells, %s'%(s, len(cells), {False:'recalc. values', True:'DB values'}[from_csv]))
     figlist.append(fig_sect)
-    
-    
+
+
 
 # Violin plot
 plt.close(1);
@@ -349,16 +349,16 @@ for i_snapshot, col_snsh, sign_shsh in zip([0,1], ['b', 'r'], [-1., 1.]):
     else:
         totintnorm = 1.
 
-    axviol.plot(2*[18+0.1*sign_shsh], [-50,(snapshots[i_snapshot]['hl_imped_sample']+snapshots[i_snapshot]['hl_sr_sample'])/totintnorm], 
+    axviol.plot(2*[18+0.1*sign_shsh], [-50,(snapshots[i_snapshot]['hl_imped_sample']+snapshots[i_snapshot]['hl_sr_sample'])/totintnorm],
         '.-', color=col_snsh, linewidth=2, markersize=10)
 
 
     for i, s in enumerate(hl.sector_list()):
         #~ print i, i_snapshot
-        axviol.fill_betweenx(y=x_hist, x1=sign_shsh*y_list[i][i_snapshot]/maxdistr*0.9+2*(i+1), x2 = 2*(i+1), 
+        axviol.fill_betweenx(y=x_hist, x1=sign_shsh*y_list[i][i_snapshot]/maxdistr*0.9+2*(i+1), x2 = 2*(i+1),
             color=col_snsh, alpha=0.5)
 
-        axviol.plot([2*(i+1)], [np.nanmean(snapshots[i_snapshot]['dict_hl_cell_by_cell'][s]['heat_loads'])/totintnorm], 
+        axviol.plot([2*(i+1)], [np.nanmean(snapshots[i_snapshot]['dict_hl_cell_by_cell'][s]['heat_loads'])/totintnorm],
             '.-', markersize=10, color=col_snsh)
 axviol.grid('on')
 
@@ -401,34 +401,34 @@ figviol.suptitle('%s'%({False:'recalc. values', True:'DB values'}[from_csv]))
 
 
 if args.o:
-    
+
     str_file = 'multiple_'
     for i_snapshot, snapshot in enumerate(snapshots):
         str_file += 'fill%dat%.2fh_'%(snapshot['filln'], snapshot['t_h'])
-        
+
     folname = args.savein+'/cellbycell_%s_%s'%(str_file, tagfname)
-    
+
     if not os.path.exists(folname):
         os.mkdir(folname)
-    
+
     if normtointen:
         str_file+='hlnorm'
     else:
         str_file+='hl'
-    
+
     for fig, s in zip(figlist, hl.sector_list()):
         fig.savefig(folname+'/cellbycell_%s_%s_sector%d.png'%(str_file, tagfname, s), dpi=200)
 
     figviol.savefig(folname+'/cellbycell_%s_%s_distrib.png'%(str_file, tagfname), dpi=200)
-    
+
     for sector in hl.sector_list():
         fname = folname+'/cellbycell_%s_%s_sector%d.csv'%(str_file, tagfname, sector)
         with open(fname, 'w') as fid:
             fid.write('cell'+''.join([',snapshot%d'%isnap for isnap in xrange(len(snapshots))])+'\n')
             for i_cell, cell in enumerate(snapshots[0]['dict_hl_cell_by_cell'][sector]['cell_names']):
                 fid.write(cell+''.join([',%.1e'%snapshots[isnap]['dict_hl_cell_by_cell'][sector]['heat_loads'][i_cell] for isnap in xrange(len(snapshots))])+'\n')
-    
-    fname = folname+'/cellbycell_%s_%s_generalinfo.txt'%(str_file, tagfname)            
+
+    fname = folname+'/cellbycell_%s_%s_generalinfo.txt'%(str_file, tagfname)
     with open(fname, 'w') as fid:
         for llll in to_table:
             fid.write('\t'.join(llll)+'\n')
