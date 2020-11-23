8b8ceddea30e323f5fca71b245df58f5f8aa13ec
giadarol <giovanni.iadarola@cern.ch>
2020-11-23 22:40:54 +0100

    Status

--- 005z_get_BSRT_calib.py
+++ 005z_get_BSRT_calib.py
@@ -1,31 +1,34 @@
-import sys, os
-BIN = os.path.expanduser("../LHC_fullRun2_analysis_scripts/")
-sys.path.append(BIN)
+from LHCMeasurementTools.LHC_Fill_LDB_Query import load_fill_dict_from_json
+from data_folders import data_folder_list, recalc_h5_folder
 
-import pickle
-
-filln = 6638
+filln = 6740
 #~ filln = 6055
 
-fills_pkl_name = '../LHC_2018_followup/fills_and_bmodes.pkl'
-with open(fills_pkl_name, 'rb') as fid:
-    dict_fill_bmodes = pickle.load(fid)
+# merge jsons and add info on location
+dict_fill_bmodes={}
+for df in data_folder_list:
+    this_dict_fill_bmodes = load_fill_dict_from_json(
+            df+'/fills_and_bmodes.json')
+    for kk in this_dict_fill_bmodes:
+        this_dict_fill_bmodes[kk]['data_folder'] = df
+    dict_fill_bmodes.update(this_dict_fill_bmodes)
 
 import pytimber
-ldb = pytimber.LoggingDB()
+#ldb = pytimber.LoggingDB(source='nxcals')
+ldb = pytimber.LoggingDB(source='ldb')
 
 
 device_name = {1:'5R4', 2:'5L4'}
 event = {450:'t_stop_INJPHYS', 6500:'t_start_STABLE'}
 
 
-e_dict = {'betaf_h':{}, 'betaf_v':{}, 'gamma':{}, 
+e_dict = {'betaf_h':{}, 'betaf_v':{}, 'gamma':{},
       'sigma_corr_h':{}, 'sigma_corr_v':{},
       'rescale_sigma_h':{}, 'rescale_sigma_v':{}, 'scale_h': {}, 'scale_v': {}}
 for kk in list(e_dict.keys()):
     e_dict[kk] = {450:{}, 6500:{}}
-    
-e_dict['gamma'][450]                = 479.6 
+
+e_dict['gamma'][450]                = 479.6
 e_dict['gamma'][6500]               = 6927.6
 
 
@@ -43,13 +46,13 @@ for ene in [450, 6500]:
                         'LHC.BSRT.%s.B%s:BETA_V'%(device_name[beam], beam)
                         ],
                         t1=t_ref, t2='last'))
-                        
+
         e_dict['betaf_h'][ene][beam] = data['LHC.BSRT.%s.B%d:BETA_H'%(device_name[beam], beam)][1][0]
         e_dict['betaf_v'][ene][beam] = data['LHC.BSRT.%s.B%d:BETA_V'%(device_name[beam], beam)][1][0]
         e_dict['sigma_corr_h'][ene][beam] = data['LHC.BSRT.%s.B%d:LSF_H'%(device_name[beam], beam)][1][0]*1e-3
         e_dict['sigma_corr_v'][ene][beam] = data['LHC.BSRT.%s.B%d:LSF_V'%(device_name[beam], beam)][1][0]*1e-3
         e_dict['rescale_sigma_h'][ene][beam] = 1.
         e_dict['rescale_sigma_v'][ene][beam] = 1.
-        
+
 print(e_dict)
 
